65518a22aaa59cce6375ce53c27e4b2efb4668df
Dvirlin <smathias476@gmail.com>
2014-06-29 19:48:18 -0400

    First Test

---
+++ src/main/java/com/dvirlin/testmod/TestMod.java
@@ -0,0 +1,32 @@
+package com.dvirlin.testmod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "TestMod", name = "Test Mod", version = "1.7.2-0.2")
+public class TestMod
+{
+    @Mod.Instance("TestMod")
+    public static TestMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+
+}
